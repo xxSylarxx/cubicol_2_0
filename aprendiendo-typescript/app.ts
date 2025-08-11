90dd0f08916c479c551a14d3dcf85acd02a603dc
Axel <axelmol2018@gmail.com>
2025-08-11 17:50:18 -0500

    avance de clase

--- aprendiendo-typescript/app.ts
+++ aprendiendo-typescript/app.ts
@@ -1,9 +1,25 @@
-const msg:string = 'Hola Mundo';
+/* const msg:string = 'Hola Mundodssxxddddddddddd';
 
 const hero = {
     name: 'Iron Man',
     age:45
 }
 
-hero.age=50
-console.log(hero.age +1);
+console.log(hero.age + 1);
+
+console.log(hero); */
+
+
+/* const a =10;
+
+function sayHello(msg:string) {
+    //console.log(msg)
+    msg.
+} */
+(() => {
+
+    const a:number=10;
+
+    console.log(a);
+
+})()
